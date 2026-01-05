e41311cba80763407af1c632afa001872f92f0e3
Phoenix <156647474+Phoenix-CSharp@users.noreply.github.com>
2026-01-05 22:14:25 +0700

    Update

--- Items/Wings/Wings.cs
+++ Items/Wings/Wings.cs
@@ -5,7 +5,6 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace PhoenixCore.Items.Wings{
-    [Autoload(false)]
     [AutoloadEquip(EquipType.Wings)]
     public class Wings : ModItem{
         public int flyTimeSec;
